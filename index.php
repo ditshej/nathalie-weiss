ba96024d7c20c8976eaeb2b6751a7ba9074cd436
Raphi W <ditshej@gmail.com>
2022-05-22 14:13:04 +0200

    create some nice starting pages

---
+++ index.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Nahli</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+
+<nav>
+    <a href="/wishlist/">
+        <span>🛍️ zur Wunschliste 🛍️</span>
+    </a>
+</nav>
+
+<div class="container">
+
+    <div class="dog">
+        <div class="dog-body">
+            <div class="dog-tail">
+                <div class="dog-tail">
+                    <div class="dog-tail">
+                        <div class="dog-tail">
+                            <div class="dog-tail">
+                                <div class="dog-tail">
+                                    <div class="dog-tail">
+                                        <div class="dog-tail"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="dog-torso"></div>
+        <div class="dog-head">
+            <div class="dog-ears">
+                <div class="dog-ear"></div>
+                <div class="dog-ear"></div>
+            </div>
+            <div class="dog-eyes">
+                <div class="dog-eye"></div>
+                <div class="dog-eye"></div>
+            </div>
+            <div class="dog-muzzle">
+                <div class="dog-tongue"></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="ball" tabindex="0"></div>
+
+</div>
+
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,5 @@
+for my love 😍
+
+# specal thanks
+## to David Khourshid
+for the CSS-Dog [https://codepen.io/davidkpiano/pen/ZpRXGj](https://codepen.io/davidkpiano/pen/ZpRXGj)
